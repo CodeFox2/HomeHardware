4b0c03db2b6eb0a5230a04f8a116c22d895c9168
CamnItAll <cameron33888@gmail.com>
2025-04-04 03:21:30 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,24 +20,25 @@ const db = mysql.createConnection({
 //ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'atthelastsecond';
 db.connect((err) => {
     if (err) throw err;
-    console.log('Connected to MySQL Database');
+    console.log('Connected to MySQL Database!');
 });
 app.get("/", (req, res) => {
     res.json("Welcome.");
 });
 
 //Post Method: Insert Data
-app.post('/add', (req, res) => {
-    const {item_name, item_price, item_quantity} = req.body;
-    const query = 'INSERT INTO cart (item_name, item_price, item_quantity) VALUES (?, ?, ?)';
-    db.query(query, [item_name, item_price, item_quantity], (err) => {
+app.post('/add', async (req, res) => {
+    const {name, email, password} = req.body;
+    console.log(req.body);
+    const query = 'INSERT INTO accounts (name, email, password) VALUES (?, ?, ?)';
+    db.query(query, [name, email, password], (err) => {
         if (err) return res.status(500).send(err);
-        res.send('Item added successfully');
+        res.send('Account added successfully');
     });
 });
 //Get Method: Read Data
-app.get('/cart', (req, res) => {
-    const query = 'SELECT * FROM cart';
+app.get('/accounts', (req, res) => {
+    const query = 'SELECT * FROM accounts';
     db.query(query, (err, results) => {
         if (err) return res.status(500).send(err);
         res.json(results);
@@ -46,19 +47,45 @@ app.get('/cart', (req, res) => {
 //Put Method: Update Data
 app.put('/update/:id', (req, res) => {
     const { id } = req.params;
-    const { item_quantity } = req.body;
-    const query = 'UPDATE cart SET item_quantity = ? WHERE id = ?';
-    db.query(query, [item_quantity, id], (err) => {
+    const { name, email, password, points } = req.body;
+    const query = 'UPDATE accounts SET name = ?, email = ?, password = ?, points = ? WHERE account_id = ?';
+    db.query(query, [name, email, password, points, id], (err) => {
         if (err) return res.status(500).send(err);
-        res.send('Item count updated successfully');
+        res.send('Item updated successfully');
     });
 });
 //Delete Method: Delete Data
 app.delete('/delete/:id', (req, res) => {
     const { id } = req.params;
-    const query = 'DELETE FROM cart WHERE id = ?';
+    const query = 'DELETE FROM account WHERE id = ?';
     db.query(query, [id], (err) => {
         if (err) return res.status(500).send(err);
         res.send('Item deleted successfully');
+    });
+});
+
+//---------------------------------------------------------------------------------------------
+
+app.post('/login', (req, res) => {
+	const {email, password} = req.params;
+	if (email && password) {
+		db.query('SELECT * FROM accounts WHERE email = ? AND password = ?', [email, password], (err, result) => {
+			if (err) return res.status(500).send(err);
+			if (result.email != "" || result.password != "") {
+				res.json(results);
+			} else {
+				res.send('Incorrect Username and/or Password!');
+			}			
+		});
+	} else {
+		res.send('Please enter Username and Password!');
+	}
+});
+
+app.get('/customer', (req, res) => {
+    const query = 'SELECT id, points FROM login';
+    db.query(query, (err, results) => {
+        if (err) return res.status(500).send(err);
+        res.json(results);
     });
 });
